25827087bafabcc2e57851377022365a95ecea88
FirasMyacoup <firas.yacoup1@gmail.com>
2023-09-12 12:49:43 +0300

    added the pictures to the image gallery

--- src/component/Gallery.js
+++ src/component/Gallery.js
@@ -2,14 +2,25 @@ import React from 'react';
 import "../CSS/gallery.css"
 
 // Import your images
-import image1 from '../assets/image1.jpg';
-import image2 from '../assets/SSEM.jpg';
-import image3 from '../assets/logo.png'
-import image4 from '../assets/logo.png'
+import image1 from '../assets/image4.jpg';
+import image2 from '../assets/image5.jpg';
+import image3 from '../assets/image6.jpg';
+import image4 from '../assets/image7.jpg';
+import image5 from '../assets/image8.jpg';
+import image6 from '../assets/image9.jpg';
+import image7 from '../assets/image10.jpg';
+import image8 from '../assets/image11.jpg';
+import image9 from '../assets/image12.jpg';
+import image10 from '../assets/image13.jpg';
+import image11 from '../assets/image14.jpg';
+import image12 from '../assets/image15.jpg';
+import image13 from '../assets/image16.jpg';
+import image14 from '../assets/image17.jpg';
+import image15 from '../assets/image18.jpg';
 
 // Import other necessary components and data
 
-const images = [image1, image2, image3,image2]; // Add more images as needed
+const images = [image1, image2, image3,image4,image5,image6,image7,image8,image9,image10,image11,image12,image13,image14,image15]; // Add more images as needed
 
 const Gallery = () => {
   return (
